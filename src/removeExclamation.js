3c312bed5e3f47752a8d0ed572883613f8615d12
georgecode <georgeczurlanis@gmail.com>
2017-07-24 19:36:18 -0700

    Add remove Exclamation algorithm

--- src/removeExclamation.js
+++ src/removeExclamation.js
@@ -1,1 +1,18 @@
-console.log("Hellllloooo");
+// Remove all exclamation marks from sentence 
+// but ensure a exclamation mark at the end of string. 
+// For a beginner kata, you can assume that the input 
+// data is always a non empty string, no need to verify it.
+
+
+// ---------examples
+// remove("Hi!") === "Hi!"
+// remove("Hi!!!") === "Hi!"
+// remove("!Hi") === "Hi!"
+// remove("!Hi!") === "Hi!"
+// remove("Hi! Hi!") === "Hi Hi!"
+// remove("Hi") === "Hi!"
+
+
+function remove(s){
+  return s.replace(/!/g, "").concat("!")
+}
