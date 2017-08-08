b49b1831ddabbdfe93e043423599453b23c241ef
georgecode <georgeczurlanis@gmail.com>
2017-08-08 01:06:17 -0700

    Convert Boolean To String problem

---
+++ src/ConvertBooleanToString.js
@@ -0,0 +1,10 @@
+console.log("ConvertBooleanToString.js")
+//booleanToString(true) //"true"
+//booleanToString(false) //"false"
+
+function booleanToString(b){
+   // var y = (x == 2 ? "yes" : "no");
+  return b == true ? "true" : "false"
+}
+
+console.log(booleanToString(true))
