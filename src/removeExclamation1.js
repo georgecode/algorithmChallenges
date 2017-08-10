c2925942b9acaeb5b95f3320f4dc33beea33264d
georgecode <georgeczurlanis@gmail.com>
2017-08-09 18:20:47 -0700

    Add remove Exclamation #1 algorithm challenge

---
+++ src/removeExclamation1.js
@@ -0,0 +1,37 @@
+console.log("removeExclamation1.js")
+
+// Remove a exclamation mark from the end of string. 
+// For a beginner kata, you can assume that the input data 
+// is always a string, no need to verify it.
+
+
+
+
+// //es5
+// function remove(s){
+//   if(s.endsWith('!') === true){
+//   	return s.slice(0,s.length-1)
+//   }else{
+//   	return s
+//   }
+// }
+
+
+// es6
+function remove(s){
+  return s.endsWith('!') === true ? s.slice(0,s.length-1) : s 
+}
+
+console.log(remove("Hi!")) //"Hi"
+console.log(remove("Hi!!!")) //"Hi!!"
+console.log(remove("!Hi")) //"!Hi"
+console.log(remove("!Hi!")) //"!Hi"
+console.log(remove("Hi! Hi!")) //"Hi! Hi"
+console.log(remove("Hi")) //"Hi"
+
+
+// var str = 'To be, or not to be, that is the question.';
+
+// console.log(str.endsWith('question.')); // true
+// console.log(str.endsWith('to be'));     // false
+// console.log(str.endsWith('to be', 19)); // true
