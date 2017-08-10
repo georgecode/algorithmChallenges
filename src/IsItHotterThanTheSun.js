abf343680367825d2fd0150d67dfd250c821bdbc
georgecode <georgeczurlanis@gmail.com>
2017-08-09 17:20:52 -0700

    Add IsItHotterThanTheSun algorithm challenge

---
+++ src/IsItHotterThanTheSun.js
@@ -0,0 +1,18 @@
+console.log("IsItHotterThanTheSun.js")
+// Your job is simple, if (x) squared is more than 1000, 
+// return 'It's hotter than the sun!!', 
+// else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+
+// X will be either a number or a string. Both are valid.
+
+function apple(x){
+	if(Math.pow(parseInt(x),2) > 1000){
+		return 'It\'s hotter than the sun!!'
+	}else{
+		return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+	}
+}
+
+
+console.log(apple('50')) // 'It\'s hotter than the sun!!'
+console.log(apple(4) ) //'Help yourself to a honeycomb Yorkie for the glovebox.');
