097a765fcdb53bfdf8749df85715890453a189d3
georgecode <georgeczurlanis@gmail.com>
2017-08-30 15:02:52 -0700

    Add solution for algorithm challenge Find Array

--- src/level7/findArray.js
+++ src/level7/findArray.js
@@ -3,12 +3,21 @@
 console.log("findArray.js")
 
 // You are given two arrays arr1 and arr2, where arr2 always contains integers.
-
 // Write the function find_array(arr1, arr2)
 
-find_array(arr1, arr2){
-
+function find_array(arr1, arr2){
+	let solution = []
+	for(let i =0;i<arr2.length;i++){
+		if(typeof arr1[arr2[i]] !== 'undefined'){
+			solution.push(arr1[arr2[i]])
+		}
+	}
+	return solution
 }
+
+// Shorter but less readable way
+// const findArray = (arr1, arr2) =>  arr2.map(e=> arr1[e]).filter(e=>e);
+
 
 console.log(find_array(['a', 'a', 'a', 'a', 'a'],[2, 4])) //['a', 'a']
 console.log(find_array([0, 1, 5, 2, 1, 8, 9, 1, 5],[1, 4, 7])) //[1, 1, 1]
