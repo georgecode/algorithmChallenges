0c19116eb9360ed6a1bf9f5d89686694d094a470
georgecode <georgeczurlanis@gmail.com>
2017-09-01 04:28:54 -0700

    Add solution for algorithm challenge sumSquares

--- src/level8/sumSquares.js
+++ src/level8/sumSquares.js
@@ -9,15 +9,39 @@ console.log("sumSquares.js")
 //Shorten the code such that it meets the requirements.
 
 // Original code provided
-function sumSquares(array) {
-  var result = 0;
-  for (let i = 0; i < array.length; i++) {
-    result += array[i] ** 2;
-  }
-  return result;
-}
+// function sumSquares(array) {
+//   var result = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     result += array[i] ** 2;
+//   }
+//   return result;
+// }
 
 
+
+// function sumSquares(array) {
+// 	return	array.reduce(function(sum, value) {
+//   		return sum + Math.pow(value,2);
+// 	}, 0);
+// }
+
+
+
+// const sumSquares = (array)=>{ 
+// 	return array.reduce((sum, value) => {
+//   		return sum + Math.pow(value,2);
+// 	},0);
+// }
+
+
+//Best way but babel is not compileing this es7 ** operator
+// const sumSquares = (array)=> 
+//	array.reduce((sum, value) => sum + value ** 2
+// 	,0);
+
+const sumSquares = (array)=> 
+	array.reduce((sum, value) => sum + Math.pow(value,2)
+	,0);
 
 
 console.log(sumSquares([1,2,3,4,5])) //55
