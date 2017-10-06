4c56c3cb008ea8d6bf5296cf455b143ba0bc2a2d
georgecode <georgeczurlanis@gmail.com>
2017-10-06 04:22:22 -0700

    Add convertHashToArray challange

---
+++ src/obj/convertHashToArray.js
@@ -0,0 +1,21 @@
+//convertHashToArray.js
+
+//babel convertHashToArray.js | node
+
+console.log("convertHashToArray.js")
+
+
+// convert this 
+// {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+
+// to this
+//  [["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
+
+function convertHashToArray(hash){
+	
+	return "hello"
+  //your code here - sort the keys!
+}
+
+let testHash = {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+console.log(convertHashToArray(testHash))
