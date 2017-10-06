7ffc926aca59b7fba6920da2ba32398c27d1c656
georgecode <georgeczurlanis@gmail.com>
2017-10-06 00:53:18 -0700

    Add makeThemBark challenge and solution

---
+++ src/obj/makeThemBark.js
@@ -0,0 +1,22 @@
+//makeThemBark
+//$ babel makeThemBark.js | node
+//The .bark() method of a dog should return the string 'Woof!'.
+
+console.log("makeThemBark")
+
+function Dog(name, breed, sex, age){
+    this.name  = name;
+    this.breed = breed;
+    this.sex   = sex;
+    this.age   = age;
+}
+
+var kona = new Dog("kona", "lab", "f", 4)
+
+console.log(kona)
+
+Dog.prototype.bark = function(){
+	console.log("Woof!")
+}
+
+kona.bark()
