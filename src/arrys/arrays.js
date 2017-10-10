d5ed895eb0f988dba6733db182d0e15783e05436
georgecode <georgeczurlanis@gmail.com>
2017-10-09 23:49:00 -0700

    Add sort example and we have lift off challange

--- src/arrys/arrays.js
+++ src/arrys/arrays.js
@@ -1,3 +1,12 @@
 // babel arrays.js | node
 
 console.log("arrays.js!!!!!")
+
+//sorting numbers
+const sortNum = (a,b) => a-b; 
+
+const sortTheNumbers = (num) => num.sort(sortNum)
+
+console.log(sortTheNumbers([8,1,10,2,7,9,6,11,20,3,4,5]))//[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20 ]
+
+//-------------------------------------------------
--- src/arrys/codewarsEasy.js
+++ src/arrys/codewarsEasy.js
@@ -14,3 +14,17 @@ console.log("codewarsEasy.js!!!!")
  var smash = (words) => words.join(' ');
 
  console.log(smash(["hello", "world"]))// "hello world"
+
+//----------------------------------------------
+ //We Have Liftoff
+
+//es5
+//  function liftoff(instructions){
+//   instructions.sort().join(' ')+ " liftoff!"
+// }
+const sortNumber = (a,b) => b-a; 
+
+const liftoff = (instructions) => instructions.sort(sortNumber).join(' ')+ " liftoff!"
+
+console.log(liftoff([8,1,10,2,7,9,6,3,4,5]))//"10 9 8 7 6 5 4 3 2 1 liftoff!"
+console.log(liftoff([1,2,4,3,5]))//"5 4 3 2 1 liftoff!"
