3108f454e163e5bea1a86fdc2e5b238891907230
georgecode <georgeczurlanis@gmail.com>
2017-10-11 21:28:21 -0700

    Add symbolsCounted.js challenge

---
+++ src/level7/symbolsCounted.js
@@ -0,0 +1,20 @@
+//$ babel symbolsCounted.js | node
+
+console.log("symbolsCounted.js")
+
+// In this kata you will have to transform each string 
+// so that it contains count for every symbol it contains, 
+// starting from 2. The order of symbols should be preserved.
+
+// Example: abbreviation => a2b2revi2ton
+
+const transform = (s) => {
+
+  return s;
+}
+
+console.log(transform('elevation'))//'e2lvation'
+console.log(transform('transplantology'))//'t2ra2n2spl2o2gy'
+console.log(transform('economics'))//'ec2o2nmis'
+console.log(transform('embarrassed'))//'e2mba2r2s2d'
+console.log(transform('impressive'))//'i2mpre2s2v'
