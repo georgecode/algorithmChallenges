2339c479c2c0c3eba385f5a1a14029a0085914f9
georgecode <georgeczurlanis@gmail.com>
2017-10-12 01:19:59 -0700

    Add symbolsCounted.js solution

--- src/level7/symbolsCounted.js
+++ src/level7/symbolsCounted.js
@@ -6,11 +6,33 @@ console.log("symbolsCounted.js")
 // so that it contains count for every symbol it contains, 
 // starting from 2. The order of symbols should be preserved.
 
+//add the number of times a letter appers after it and remove it from the string
+
 // Example: abbreviation => a2b2revi2ton
 
 const transform = (s) => {
+ 	let newArray = []
+ 	let repeats = ""
 
-  return s;
+	s.split('').forEach((i)=>{
+		let regLetterG = new RegExp(i,"g")
+		let letterOccur =(s.match(regLetterG) || []).length
+
+		let regLetter = new RegExp(i)
+		let RepeatTest = regLetter.test(repeats);
+
+		if(!RepeatTest){
+			if(letterOccur>1){
+				newArray.push(i + letterOccur)
+				repeats=repeats.concat(i)
+			}else{
+				newArray.push(i)
+			}			
+		}
+
+	})//End forEach
+	return newArray.join('')
+  
 }
 
 console.log(transform('elevation'))//'e2lvation'
