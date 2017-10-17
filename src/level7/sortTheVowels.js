c5bee36e2a8dd3b2403248d9d4b93c5c5179a2c8
georgecode <georgeczurlanis@gmail.com>
2017-10-16 20:32:05 -0700

    Add sortTheVowels Challenge

---
+++ src/level7/sortTheVowels.js
@@ -0,0 +1,19 @@
+//sortTheVowels.js
+//$ babel sortTheVowels.js | node
+
+console.log("sortTheVowels.js!!!!!")
+
+// function sortVowels(s){
+//   //code
+// }
+
+const sortVowels = (s)=>{
+ return "hello"
+}
+
+
+
+console.log(sortVowels('Codewars'))//'C|\n|o\nd|\n|e\nw|\n|a\nr|\ns|'
+console.log(sortVowels('Rnd Te5T'))//'R|\nn|\nd|\n |\nT|\n|e\n5|\nT|'
+//console.log(sortVowels(1337))//''
+//console.log(sortVowels(undefined))//''
