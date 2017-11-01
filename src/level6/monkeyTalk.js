631b7db383d3d4dd4f88f6064171163e9e62938a
georgecode <georgeczurlanis@gmail.com>
2017-10-31 22:58:40 -0700

    Add monkeyTalk.js challenge

---
+++ src/level6/monkeyTalk.js
@@ -0,0 +1,16 @@
+//$ babel monkeyTalk.js | node
+
+console.log("monkeyTalk.js!!!!")
+
+
+
+function monkeyTalk(phrase){
+
+
+  return phrase
+}
+
+console.log(monkeyTalk('Hello'))//'Ook.'
+console.log(monkeyTalk('Everyone'))//'Eek.'
+console.log(monkeyTalk('Hello Everyone'))//'Ook eek.'
+console.log(monkeyTalk('Everyone Hello'))//'Eek ook.'
