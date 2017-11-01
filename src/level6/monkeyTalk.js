3e72d3248d5fe974d24ff1454a8a0745e633b1f7
georgecode <georgeczurlanis@gmail.com>
2017-11-01 00:50:27 -0700

    Add Monkey Talk solution

--- src/level6/monkeyTalk.js
+++ src/level6/monkeyTalk.js
@@ -2,12 +2,23 @@
 
 console.log("monkeyTalk.js!!!!")
 
-
+//var res = str.replace(/blue/g, "red");
 
 function monkeyTalk(phrase){
+	// phrase =phrase.split(' ')
 
+	let monkey = phrase.split(' ').map((i)=>{
+		return i[0].match(/[aeiou]/i)? 'eek':'ook'
+	})
 
-  return phrase
+	monkey[0]= monkey[0].charAt(0).toUpperCase() + monkey[0].slice(1)
+
+	// let monkey = phrase.replace(/hello/gi, "ook")
+	// .replace(/everyone/gi, "eek") + "."
+
+ //  	return monkey.charAt(0).toUpperCase() + monkey.slice(1)
+ return monkey.join(' ') + "."
+	
 }
 
 console.log(monkeyTalk('Hello'))//'Ook.'
--- src/level7/symbolsCounted.js
+++ src/level7/symbolsCounted.js
@@ -40,3 +40,16 @@ console.log(transform('transplantology'))//'t2ra2n2spl2o2gy'
 console.log(transform('economics'))//'ec2o2nmis'
 console.log(transform('embarrassed'))//'e2mba2r2s2d'
 console.log(transform('impressive'))//'i2mpre2s2v'
+
+
+
+
+
+
+
+
+
+
+
+
+
