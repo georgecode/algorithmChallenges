49f2b663044b7c3963e3aa3b3c0434ad3fef3194
georgecode <georgeczurlanis@gmail.com>
2018-02-20 19:54:23 -0800

    Add csvOfArray solution

--- src/level8/csvOfArray.js
+++ src/level8/csvOfArray.js
@@ -2,23 +2,42 @@
 
 console.log("csvOfArray.js")
 
+//my version
+// const toCsvText = (array)=>{
+// 	let str ="";
+// 	array.forEach((arr)=>{
+// 		str += arr.toString()+"\n"
+// 	})
+// 	return str.trim()
+// };
+
+//better version
 function toCsvText(array) {
-	console.log("hello")
-	return "hello"
-   // good luck
+   return array.join('\n');
 }
 
 
-const arrTest= [ 0, 1, 2, 3, 4 ]
+//const arrTest= [ 0, 1, 2, 3, 4 ]
 
 const arr1 = [[ 0, 1, 2, 3, 4 ],
     [ 10,11,12,13,14 ],
     [ 20,21,22,23,24 ],
     [ 30,31,32,33,34 ]]
 
-console.log(toCsvText(arrTest))//  
+//console.log(toCsvText(arrTest))//  
 console.log(toCsvText(arr1))//    
 	// '0,1,2,3,4\n'
  //    +'10,11,12,13,14\n'
  //    +'20,21,22,23,24\n'
  //    +'30,31,32,33,34'
+
+
+
+
+
+
+
+
+
+
+
