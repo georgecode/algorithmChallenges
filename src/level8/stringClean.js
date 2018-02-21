7a48b68d03eb8823b66461b093e32d7ad39bad1d
georgecode <georgeczurlanis@gmail.com>
2018-02-20 20:04:03 -0800

    Get rid of numbers

---
+++ src/level8/stringClean.js
@@ -0,0 +1,13 @@
+//babel stringClean.js | node
+
+//Get rid of numbers
+
+console.log("stringClean.js")
+
+function stringClean(s){
+  // Function will return the cleaned string
+}
+
+console.log(stringClean("! !"))//"! !"
+console.log(stringClean("123456789"))//""
+console.log(stringClean("(E3at m2e2!!)"))//"(Eat me!!)"
