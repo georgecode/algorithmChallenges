e77d58e85dfdf1424e1650f9d18d57954d24dcda
georgecode <georgeczurlanis@gmail.com>
2018-02-20 20:17:49 -0800

    Add removeFirstandLast problem

---
+++ src/level8/removeFirstandLast.js
@@ -0,0 +1,13 @@
+//babel removeFirstandLast.js | node
+
+console.log("removeFirstandLast.js")
+
+function removeChar(str){
+ //You got this!
+ return str
+
+};
+
+console.log(removeChar('eloquent'))//'loquen'
+console.log(removeChar('country'))//'ountr'
+console.log(removeChar('person'))//'erso'
