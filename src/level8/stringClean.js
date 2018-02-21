c6964316ce71f04ee9d5dd2a96f8421a2cf0e794
georgecode <georgeczurlanis@gmail.com>
2018-02-20 20:11:55 -0800

    Add stringClean solution

--- src/level8/stringClean.js
+++ src/level8/stringClean.js
@@ -4,9 +4,12 @@
 
 console.log("stringClean.js")
 
-function stringClean(s){
-  // Function will return the cleaned string
-}
+// function stringClean(s){
+// 	return s.replace(/[0-9]/,"")
+//   // Function will return the cleaned string
+// }
+
+const stringClean=(s)=>  s.replace(/[0-9]/g,"")
 
 console.log(stringClean("! !"))//"! !"
 console.log(stringClean("123456789"))//""
