0872d56adf392aa79dd170e69be787b5555f431b
georgecode <georgeczurlanis@gmail.com>
2018-02-20 20:45:35 -0800

    Add solution for removeFirstandLast

--- src/level8/removeFirstandLast.js
+++ src/level8/removeFirstandLast.js
@@ -2,11 +2,17 @@
 
 console.log("removeFirstandLast.js")
 
-function removeChar(str){
- //You got this!
- return str
+// function removeChar(str){
+//  //You got this!
+//  return str.slice(1,str.length).slice(0,-1)
 
-};
+// };
+
+//mine
+const removeChar =(str)=> str.slice(1,str.length).slice(0,-1)
+
+//best pratice
+const removeChar = str => str.slice(1,-1)
 
 console.log(removeChar('eloquent'))//'loquen'
 console.log(removeChar('country'))//'ountr'
