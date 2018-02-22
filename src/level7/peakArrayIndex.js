9ad03615a131ce7af6f5a081e09a7dac1fe31b54
georgecode <georgeczurlanis@gmail.com>
2018-02-21 17:24:16 -0800

    Add peakArrayIndex problem

---
+++ src/level7/peakArrayIndex.js
@@ -0,0 +1,15 @@
+//babel peakArrayIndex.js | node
+console.log("peakArrayIndex.js")
+
+// Given an array of ints, return the index such that the sum 
+// of the elements to the right of that index equals the sum 
+// of the elements to the left of that index. If there is no 
+// such index, return -1.
+
+function peak(arr){
+ return arr
+}
+
+console.log(peak([1,2,3,5,3,2,1]))//3
+console.log(peak([1,12,3,3,6,3,1]))//2
+console.log(peak([10,20,30,40]))//-1
