9eeefa9bdccda6ee69e51bc575e0264881dd3c5e
georgecode <georgeczurlanis@gmail.com>
2018-02-22 17:48:10 -0800

    Add solution for listofPresents

--- src/level7/listofPresents.js
+++ src/level7/listofPresents.js
@@ -9,15 +9,34 @@ console.log("listofPresents.js")
 // (a list in Groovy) containing the price of each gift. 
 // You should return an integer representing the maximum amount of gifts Leo can buy.
 
-
-function howManyGifts(maxBudget, gifts){
-	console.log(gifts)
-	return maxBudget
+//My solution
+const howManyGifts=(maxBudget, gifts)=>{
+	gifts = gifts.sort((a, b)=>{return a - b});
+	let total=0;
+	for(let i = 0;i<gifts.length;i++){
+		total+=gifts[i]
+		if(total>maxBudget){
+			return i
+		}
+	}
+	return gifts.length	
 }
 
+// //better solution
+// function howManyGifts(m, g){
+//   var i = 0;
+//   g = g.sort(function(a, b){return a - b});
+//   for(; m - g[i] >= 0; i++){
+//     m = m - g[i];
+//   }
+//   return i;
+// }
 
 
 
 console.log(howManyGifts(20, [13, 2, 4, 6, 1]))//4
 console.log(howManyGifts(0, [1]))//0
 //console.log(howManyGifts(maxBudget, gifts))//
+
+
+
