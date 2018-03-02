e1d408d0a847bb8375658317b9f659c5823db0f5
georgecode <georgeczurlanis@gmail.com>
2018-03-01 16:20:59 -0800

    Add missingArithmetic challenge

---
+++ src/level5/missingArithmetic.js
@@ -0,0 +1,28 @@
+//babel missingArithmetic.js | node
+
+console.log("missingArithmetic.js")
+
+
+// An Arithmetic Progression is defined as one 
+// in which there is a constant difference between 
+// the consecutive terms of a given series of numbers. 
+// You are provided with consecutive elements of an 
+// Arithmetic Progression. There is however one hitch:
+// exactly one term from the original series is 
+// missing from the set of numbers which have been 
+// given to you. The rest of the given series is the 
+// same as the original AP. Find the missing term.
+
+// You have to write the function findMissing(list), 
+// list will always be at least 3 numbers. The missing 
+// term will never be the first or last one.
+
+
+
+
+var findMissing = function (list) {  
+  return list;
+}
+
+
+console.log(findMissing([1, 3, 4]))//2
